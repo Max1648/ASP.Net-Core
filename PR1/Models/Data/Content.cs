3c9df7eb88f8bce7ae2c2b11954d3d0b79a44b93
Max Nadvorniy <ipzk201_nmyu@student.ztu.edu.ua>
2021-02-09 22:01:37 +0200

    PR1

---
+++ PR1/Models/Data/Content.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PR1.Models;
+
+namespace PR1.Models.Data
+{
+    public class Content : DbContext
+    {
+        public Content(DbContextOptions<Content> options)
+           : base(options)
+        {
+        }
+
+        public DbSet<Task> Task { get; set; }
+    }
+}
---
+++ PR1/Models/Task.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PR1.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+    }
+}
